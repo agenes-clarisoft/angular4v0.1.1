da120784e8bbae3308d119058076cfebeb60953a
agenes-clarisoft <andrei.genes@clarisoft.com>
2017-12-05 17:59:10 +0200

    Heroes List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero/hero-detail.component';
+import { HeroListComponent } from './hero/hero-list.component';
 
+import { HeroService } from './services/hero.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
